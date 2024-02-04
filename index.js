f883ee7b32bd22395bd2703e5d7eb04fc383147c
jennifer <106498069+jennyxs@users.noreply.github.com>
2024-02-03 20:06:39 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const quotations = [
-    "QYou have to believe in yourself when no one else does.",
+    "You have to believe in yourself when no one else does.",
     "The only thing we have to fear is fear itself.",
     "Confident people have a way of carrying themselves that makes others attracted to them.”"
   ];
